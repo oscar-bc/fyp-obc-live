bd14f79ed46c9c95942cfe7ef65e0c70c025bfc6
oscar-bc <44496408+oscar-bc@users.noreply.github.com>
2021-03-22 17:46:10 +0000

    Add files via upload

--- FYP_django_app/settings.py
+++ FYP_django_app/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'FYP_django_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
         # 'ENGINE': 'django.db.backends.mysql',
         # 'HOST': 'fyp-django-308317:europe-west2:fyp-instance',
         # 'USER': 'oscar',
